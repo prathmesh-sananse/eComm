ee543532c9618b0948c2125582f68bba8b4f2911
Prathmesh Sananse <113549833+prathmesh-sananse@users.noreply.github.com>
2025-11-01 07:49:06 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import cors from 'cors';
 
 const port = process.env.PORT || 5000;
 
@@ -16,6 +17,13 @@ connectDB();
 
 const app = express();
 
+app.use(
+  cors({
+    origin: ['https://e-comm-ftend.vercel.app'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
