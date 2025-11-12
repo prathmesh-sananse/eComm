9bb669367050d93921087ddf2b0668a725ec4b09
Prathmesh Sananse <113549833+prathmesh-sananse@users.noreply.github.com>
2025-11-12 11:20:36 +0530

    Update constants.js

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,9 +1,7 @@
 export const BASE_URL =
-  process.env.NODE_ENV === 'development'
-    ? 'http://localhost:5000' // for local testing
-    : 'https://ecomm-13456.onrender.com'; // for Vercel (production)
-//export const BASE_URL = ''; // If using proxy
-export const PRODUCTS_URL = '/api/products';
-export const USERS_URL = '/api/users';
-export const ORDERS_URL = '/api/orders';
-export const PAYPAL_URL = '/api/config/paypal';
+  process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000';
+
+export const PRODUCTS_URL = `${BASE_URL}/api/products`;
+export const USERS_URL = `${BASE_URL}/api/users`;
+export const ORDERS_URL = `${BASE_URL}/api/orders`;
+export const PAYPAL_URL = `${BASE_URL}/api/config/paypal`;
